61fc938c61b2be7b9331a56cae8c008f040e1b39
hunaa2006 <hunaahun0@gmail.com>
2026-02-09 22:35:46 +0800

    Ignore build files

--- java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
+++ java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
@@ -1,13 +1,19 @@
 package edu.cmu.cs.cs214.lab02;
 
 import edu.cmu.cs.cs214.lab02.shapes.Rectangle;
+import edu.cmu.cs.cs214.lab02.shapes.Shape;
 
 public class Main {
+    // Main класс дээр Rectangle объектыг Shape интерфейсээр дамжуулж өгсөн.
+    // Энэ нь битүүмжлэл болон мэдээлэл нууцлалыг сайжруулж,
+    // Renderer классыг тодорхой нэг дүрсээс хараат бус болгож байгаа.
+    // Шинэ дүрс нэмэхэд Main болон Renderer код өөрчлөгдөх шаардлагагүй болно.
+
     public static void main(String[] args) {
-        Rectangle rectangle = new Rectangle(2, 3);
-
-        Renderer renderer = new Renderer(rectangle);
-
+        Shape rect = new Rectangle(5, 10);
+        Renderer renderer = new Renderer(rect);
         renderer.draw();
+
+        
     }
 }
